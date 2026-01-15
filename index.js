f21c90ac296e087dbafb64ad0fee811e27dca850
Santiago <santyagoplay@gmail.com>
2026-01-14 21:32:09 -0500

    Primer app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My-app");
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.get("/", (req, res) =>{
+  res.send("Hello world!")
+});
+
+app.listen(port,() =>{
+  console.log("Servidor corriendo en el puerto " + port);
+});
