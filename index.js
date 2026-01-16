3c64813f8e0c939c2f42a638b47d57209e9edf03
Santiago <santyagoplay@gmail.com>
2026-01-16 13:23:07 -0500

    Agregando primeras rutas 'routing'

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) =>{
   res.send("Hello world!")
 });
 
+app.get("/nueva-ruta", (req, res) =>{
+  res.send("Nueva ruta!!!!!!!!!!!!")
+});
+
+app.get("/products", (req, res) =>{
+  res.json({
+    name: "producto 1",
+    price: 1000
+  })
+});
+
 app.listen(port,() =>{
   console.log("Servidor corriendo en el puerto " + port);
 });
